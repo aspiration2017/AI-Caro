0abb17fa8218eaa9e2cb351b49e6d11f3f2d7fc9
unknown <nhoktuyenpro@gmail.com>
2019-04-16 19:34:50 +0700

    create class computerPlayer

---
+++ src/model/ComputerPlayer.java
@@ -0,0 +1,25 @@
+package model;
+
+public class ComputerPlayer implements IPlayer {
+	int id = 2;
+	BoardState boardArr;
+
+	public ComputerPlayer(BoardState boardArr) {
+		super();
+		this.boardArr = boardArr;
+	}
+	
+	@Override
+	public boolean move(Chessman chessman) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
