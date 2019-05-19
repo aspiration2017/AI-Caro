abd6c361ce349a47bd43c3c822b74855ca9675d2
Bùi Đình Tuyên <nhoktuyenpro@gmail.com>
2019-05-19 20:24:38 +0700

    action for computer

--- src/model/ComputerPlayer.java
+++ src/model/ComputerPlayer.java
@@ -2,23 +2,31 @@ package model;
 
 public class ComputerPlayer implements IPlayer {
 	int id = 2;
-	BoardState boardArr;
+	BoardState board;
 
 	public ComputerPlayer(BoardState boardArr) {
 		super();
-		this.boardArr = boardArr;
+		this.board = boardArr;
 	}
 	
 	@Override
 	public boolean move(Chessman chessman) {
-		// TODO Auto-generated method stub
-		return false;
+		board.setPosition(chessman);
+		board.lastMove = chessman;
+		board.count++;
+		return true;
 	}
 
 	@Override
 	public int getId() {
 		return id;
 	}
-
-
+	
+	public Chessman getProfitableMove(int idHumanPlayer) {
+		int[][] scoreBoard = EvaluateBoard.evaluate(board.boardArr, id, idHumanPlayer);
+		Chessman c = EvaluateBoard.getMax(scoreBoard);
+		c.setPlayerID(id);
+		System.out.println( c.x + "-" + c.y + ": " +scoreBoard[c.x][c.y]);
+		return c;
+	}
 }
--- src/model/IPlayer.java
+++ src/model/IPlayer.java
@@ -3,5 +3,5 @@ package model;
 public interface IPlayer {
 	public boolean move(Chessman chessman);
 	public int getId();
-		
+	public Chessman getProfitableMove(int idCompetitor);	
 }
